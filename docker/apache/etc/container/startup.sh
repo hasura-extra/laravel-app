fc6f63a33deb225bee65db6366b4629d3cef8195
Minh Vuong <vuongxuongminh@gmail.com>
2021-12-03 00:11:09 +0700

    fix: startup script when initial project.

--- docker/apache/etc/container/startup.sh
+++ docker/apache/etc/container/startup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 # optimize config (caching routes and config).
-php artisan optimize
+if [[ -f "vendor/autoload.php" ]]; then
+    php artisan optimize
+fi
 
 # include your extra logic before exec command.
